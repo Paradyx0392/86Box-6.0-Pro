f906c84c7edd5c30d9e525c1a9fff928df4fec67
Paradyx0392 <paradyx0392@gmail.com>
2026-06-05 21:02:02 +0800

    Update machine.h

--- src/include/86box/machine.h
+++ src/include/86box/machine.h
@@ -1332,7 +1332,7 @@ extern int             machine_at_ax6bc_init(const machine_t *);
 #ifdef EMU_DEVICE_H
 extern const device_t  p2bb_device;
 #endif
-extern int             machine_at_ax6bc_init(const machine_t *);
+extern int             machine_at_p2bb_init(const machine_t *);
 #ifdef EMU_DEVICE_H
 extern const device_t  p2bls_device;
 #endif
